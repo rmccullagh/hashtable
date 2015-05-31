7da1e8e56b86f472aa5afe699622a2cf8fc78ccc
Ryan McCullagh <me@ryanmccullagh.com>
2015-05-31 14:39:15 -0500

    add year to copyright header

--- src/var.h
+++ src/var.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) Ryan McCullagh <me@ryanmccullagh.com>
+ *  Copyright (c) 2015 Ryan McCullagh <me@ryanmccullagh.com>
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
